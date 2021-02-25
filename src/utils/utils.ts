3f8280c6e1737972c035e88fe3e0369afa31b818
Sergey Naumenko <sergeynau@gmail.com>
2021-02-26 02:03:00 +0300

    feat: move matrix functionality to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,21 +1,125 @@
-import { isNull } from "util";
+import { isNull, isNumber } from "util";
+import { BoardType, TileType } from '../types';
 
 const getRandomIndex = (max: number) => {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-const createMatrix = (array: (React.FC | null)[], size: number) => {
-  const matrix = [];
-  for (let i = 0; i < array.length; i += size) {
-    const row = [];
-    for (let j = i; j < i + size; j++) {
-      row.push(array[j]);
+const shiftRow = (toLeft: boolean, matrix: (number)[][], tiles: TileType[]) => {
+
+}
+
+const shiftMatrix = (direction: string, board: BoardType) => {
+  console.log('shift matrix')
+  const { matrix, tiles } = board;
+
+  const matrixSize = matrix.length;
+  const newMatrix = [];
+  const newTiles = tiles.map((tile) => Object.assign({}, tile));
+  
+  for (let rowIndex = 0; rowIndex < matrixSize; rowIndex++) {
+    newMatrix.push([...matrix[rowIndex]]);
+    
+    for (let i = 0; i < matrixSize - 1; i++) {
+      for(let j = i + 1; j < matrixSize; j++) {
+        const tile1Key = newMatrix[rowIndex][i];
+        const tile2Key = newMatrix[rowIndex][j];
+        
+        if (isNull(tile1Key) && !isNull(tile2Key)) {
+          newMatrix[rowIndex][i] = tile2Key;
+          newMatrix[rowIndex][j] = null;
+
+          if (tile2Key) {
+            const tileIndex = newTiles.findIndex((item) => item.key === tile2Key);
+            newTiles[tileIndex].row = rowIndex;
+            newTiles[tileIndex].column = i;
+          }
+          continue;
+        }
+        if (!isNull(tile1Key) && !isNull(tile2Key)){
+          const tile1Index = newTiles.findIndex((item) => item.key === tile1Key);
+          const tile2Index = newTiles.findIndex((item) => item.key === tile2Key);
+          
+          console.log(newTiles[tile1Index]);
+          console.log(newTiles[tile2Index]);
+
+          if (newTiles[tile1Index].value === newTiles[tile2Index].value) {
+            console.log(newTiles[tile1Index].value);
+            newTiles[tile1Index].value = newTiles[tile1Index].value * 2;
+            console.log(newTiles[tile1Index].value);
+
+
+            newTiles[tile2Index].isVisible = false;
+            newTiles[tile2Index].row = rowIndex;
+            newTiles[tile2Index].column = i;
+            
+            newMatrix[rowIndex][j] = null;
+            continue;
+          }
+        }
+      }
     }
-    matrix.push([...row]);
   }
-  return matrix;
+
+  return {
+    matrix: newMatrix,
+    tiles: newTiles,
+  };
+}
+
+const removeInvisible = ({matrix, tiles}:BoardType) => {
+  const newTiles = tiles.map((tile) => Object.assign({}, tile));
+  return {
+    matrix,
+    tiles: [...newTiles.filter((tile) => tile.isVisible)],
+  };
+}
+
+const createTile = (value:number, row:number, column:number) => {
+  return {
+    key: new Date().getTime(),
+    row,
+    column,
+    value,
+    isVisible: true,
+    isNew: true
+  }
+}
+
+const addTile = (board: BoardType) => {
+  const { matrix, tiles } = board;
+  const boardSize = matrix.length;
+  const nullIndexes = matrix.flat()
+                        .map((value, index) => isNull(value) ? index : null)
+                        .filter((value) => value != null);
+
+  if (nullIndexes.length) {
+    const randIndex = nullIndexes[getRandomIndex(nullIndexes.length)];
+    if (isNumber(randIndex)) {
+      const newBoard = matrix.map((row) => row.map((column) => column));
+
+      const row = Math.trunc(randIndex / boardSize);
+      const column = randIndex % boardSize;
+      
+      const newTile = createTile(2, row, column);
+
+      const newTiles = tiles.map((tile) => Object.assign({}, tile));
+      newTiles.push(newTile);
+      
+      newBoard[row][column] = newTile.key;
+
+      return {
+        matrix: newBoard,
+        tiles: newTiles,
+      }
+    }
+  }
+  return board;
 }
 
 export {
   getRandomIndex,
+  removeInvisible,
+  shiftMatrix,
+  addTile,
 }
